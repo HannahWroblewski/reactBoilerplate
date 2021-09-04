6885ded3979ef37eb4fa6f6a4c376b9c7b65b19b
HannahWroblewski <hannah.wrob@gmail.com>
2021-09-04 12:03:51 -0400

    my first commit

---
+++ src/api/index.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const BASE = 'https://jsonplace-univclone.herokuapp.com'
+
+// this is an example of an api call with axios
+export async function getUsers() {
+  try {
+    const { data } = await axios.get(`${ BASE }/users`);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
